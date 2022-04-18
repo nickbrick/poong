bab05ca5e2a8a4b026b96ff403b812bf7775a915
Nick Kamzelas <nickbrick@hotmail.com>
2022-04-18 03:23:48 +0300

    Fix Point.operator - return type

--- Poong.Engine/Math/Point.cs
+++ Poong.Engine/Math/Point.cs
@@ -14,7 +14,7 @@
             X = xy;
             Y = xy;
         }
-        public static Point operator -(Point a, Point b)
+        public static Vector operator -(Point a, Point b)
         {
             return new Vector(a.X - b.X, a.Y - b.Y);
         }
