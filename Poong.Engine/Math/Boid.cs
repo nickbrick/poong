83d9711440ea5bc075fbb890441b21e1f9509106
Nick Kamzelas <nickbrick@hotmail.com>
2022-05-18 21:38:52 +0300

    Complexify Boid

--- Poong.Engine/Math/Boid.cs
+++ Poong.Engine/Math/Boid.cs
@@ -6,31 +6,28 @@ namespace Poong.Engine
 {
     internal class Boid : Body
     {
-        public Player Player { get; set; }
-        public float ConvergePower { get; set; }
-        public float AlignPower { get; set; }
-        public float AvoidPower { get; set; }
+        public Player Player { get; }
+        public float ConvergePower { get; }
         public Point FlockCenterOffset { get; }
+        public float MaximumSpeed { get; }
+        public float SpeedDecay { get; }
         public Boid(Player player)
-            : base(player.Position, new Size(0), new Vector(RollStat(0.01f, 0.5f), RollStat(0.01f, 0.5f)))
+            : base(player.Position, new Size(0), new Vector(RollStat(0.0051f, 0.5f), RollStat(0.0051f, 0.5f)))
         {
             Player = player;
-            ConvergePower = RollStat(0.01f, 0.951f);
-            AlignPower = RollStat(0.00f, 0.951f);
-            AvoidPower = RollStat(10.1021f, 0.951f);
-            FlockCenterOffset = new Point(1-RollStat(1.0f, 0.5f), 1-RollStat(1.0f, 0.5f));
-        }
-
-        internal float GetDistance(Boid neighbor)
-        {
-            return MathF.Sqrt(MathF.Pow(Center.X - neighbor.Center.X, 2) + MathF.Pow(Center.Y - neighbor.Center.Y, 2));
+            ConvergePower = RollStat(0.02f, 0.951f);
+            FlockCenterOffset = new Point(1 - RollStat(1.0f, 0.5f), 1 - RollStat(1.0f, 0.5f));
+            MaximumSpeed = RollStat(0.5f, 0.1f);
+            SpeedDecay = MathF.Min(0.99f, RollStat(0.99f, 0.05f));
         }
         internal override void Update(long time)
         {
-            Speed.X += RollStat(0.0051f / MathF.Max(0.001f, Speed.X) / 10000, 0.1f);
-            Speed.Y += RollStat(0.0051f / MathF.Max(0.001f, Speed.Y) / 10000, 0.1f);
-            Speed.X = Math.Clamp(Speed.X, -0.01f, 0.01f);
-            Speed.Y = Math.Clamp(Speed.Y, -0.51f, 0.51f);
+            Speed.X += RollStat(0.0051f / MathF.Max(0.001f, Speed.X) / 10000, 0.1f) * Math.Sign(new Random().NextDouble() - 0.5);
+            Speed.Y += RollStat(0.0051f / MathF.Max(0.001f, Speed.Y) / 10000, 0.1f) * Math.Sign(new Random().NextDouble() - 0.5);
+            Speed.X = Math.Clamp(Speed.X, -MaximumSpeed, MaximumSpeed);
+            Speed.Y = Math.Clamp(Speed.Y, -MaximumSpeed, MaximumSpeed);
+            Speed.X *= SpeedDecay * (1 - Math.Abs(Speed.X / MaximumSpeed));
+            Speed.Y *= SpeedDecay * (1 - Math.Abs(Speed.Y / MaximumSpeed));
             base.Update(time);
         }
         private static float RollStat(float mean, float relativeError)
