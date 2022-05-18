5fcbf2229cdd8dd62eb98bb363e44f6b668644ee
Nick Kamzelas <nickbrick@hotmail.com>
2022-05-18 21:38:44 +0300

    Simplify Flock

--- Poong.Engine/Math/Flock.cs
+++ Poong.Engine/Math/Flock.cs
@@ -10,8 +10,6 @@ namespace Poong.Engine
         public List<Boid> Boids { get; set; }
         public float X = 0;
         public float Y = 0;
-        public float Vx = 0;
-        public float Vy = 0;
 
         public Flock()
         {
@@ -19,33 +17,17 @@ namespace Poong.Engine
         }
         public void Update()
         {
-            if (Boids.Count > 0)
-            {
-                Vx = Boids.Average(boid => boid.Speed.X);
-                Vy = Boids.Average(boid => boid.Speed.Y);
-            }
             foreach (var boid in Boids)
             {
-                (float convergeVx, float convergeVy) = Converge(boid, boid.ConvergePower);
-                (float alignVx, float alignVy) = Align(boid, boid.AlignPower);
-                boid.Speed.X += (convergeVx + alignVx);
-                boid.Speed.Y += (convergeVy + alignVy);
-                boid.Speed.X = Math.Clamp(boid.Speed.X * 0.99f, -0.021f, 0.021f);
-                boid.Speed.Y = Math.Clamp(boid.Speed.Y * 0.99f, -0.021f, 0.021f);
+                boid.Speed += GetConvergeSpeed(boid, boid.ConvergePower);
                 boid.Update(0);
             }
         }
-        private (float Vx, float Vy) Converge(Boid boid, float power)
+        private Vector GetConvergeSpeed(Boid boid, float power)
         {
             float dVx = X + boid.FlockCenterOffset.X - boid.Center.X;
             float dVy = Y + boid.FlockCenterOffset.Y - boid.Center.Y;
-            return (dVx * power, dVy * power);
-        }
-        private (float Vx, float Vy) Align(Boid boid, float power)
-        {
-            float dVx = Vx - boid.Speed.X;
-            float dVy = Vy - boid.Speed.Y;
-            return (dVx * power, dVy * power);
+            return new Vector(dVx * power, dVy * power);
         }
     }
 }
