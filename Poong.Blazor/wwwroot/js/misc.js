f790c41242086f44d13aa6bd950e4703afd147d6
Nick Kamzelas <nickbrick@hotmail.com>
2022-05-31 22:13:15 +0300

    Fix black background flag

--- Poong.Blazor/wwwroot/js/misc.js
+++ Poong.Blazor/wwwroot/js/misc.js
@@ -8,15 +8,15 @@ function setDocumentTitle(title) {
     document.title = title;
 }
 
-backgroundIsBlack = false;
+backgroundIsColor = true;
 hue = 300;
 function rollColor() {
     hue = Math.floor(Math.random() * (540 - 300 + 1) + 300);
     document.documentElement.style.setProperty('--body-color', `hsl(${hue},100%,50%)`);
-    document.documentElement.style.setProperty('--background-color', `hsl(${hue},60%,${3 * backgroundIsBlack}%)`);
+    document.documentElement.style.setProperty('--background-color', `hsl(${hue},60%,${3 * backgroundIsColor}%)`);
     setContextStyles();
 }
 function clearBackground() {
-    backgroundIsBlack ^= true;
-    document.documentElement.style.setProperty('--background-color', `hsl(${hue},60%,${3 * backgroundIsBlack}%)`);
+    backgroundIsColor ^= true;
+    document.documentElement.style.setProperty('--background-color', `hsl(${hue},60%,${3 * backgroundIsColor}%)`);
 }
