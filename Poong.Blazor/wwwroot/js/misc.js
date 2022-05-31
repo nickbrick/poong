dec736cca70f89d13e1fec00aeceecc4a1dd68a3
Nick Kamzelas <nickbrick@hotmail.com>
2022-05-31 19:49:56 +0300

    Add background-color

--- Poong.Blazor/wwwroot/js/misc.js
+++ Poong.Blazor/wwwroot/js/misc.js
@@ -9,6 +9,8 @@ function setDocumentTitle(title) {
 }
 
 function rollColor() {
-    document.documentElement.style.setProperty('--body-color', `hsl(${Math.floor(Math.random() * (540 - 300 + 1) + 300)},100%,50%`);
+    var hue = Math.floor(Math.random() * (540 - 300 + 1) + 300);
+    document.documentElement.style.setProperty('--body-color', `hsl(${hue},100%,50%)`);
+    document.documentElement.style.setProperty('--background-color', `hsl(${hue},60%,3%)`);
     setContextStyles();
 }
