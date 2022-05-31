29135fa6e4b043526d5ccbb51cfbe67db77d11be
Nick Kamzelas <nickbrick@hotmail.com>
2022-05-31 22:06:22 +0300

    Add black screen on right click

--- Poong.Blazor/wwwroot/js/misc.js
+++ Poong.Blazor/wwwroot/js/misc.js
@@ -8,9 +8,15 @@ function setDocumentTitle(title) {
     document.title = title;
 }
 
+backgroundIsBlack = false;
+hue = 300;
 function rollColor() {
-    var hue = Math.floor(Math.random() * (540 - 300 + 1) + 300);
+    hue = Math.floor(Math.random() * (540 - 300 + 1) + 300);
     document.documentElement.style.setProperty('--body-color', `hsl(${hue},100%,50%)`);
-    document.documentElement.style.setProperty('--background-color', `hsl(${hue},60%,3%)`);
+    document.documentElement.style.setProperty('--background-color', `hsl(${hue},60%,${3 * backgroundIsBlack}%)`);
     setContextStyles();
+}
+function clearBackground() {
+    backgroundIsBlack ^= true;
+    document.documentElement.style.setProperty('--background-color', `hsl(${hue},60%,${3 * backgroundIsBlack}%)`);
 }
